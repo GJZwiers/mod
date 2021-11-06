f8acba889b5e554a1a8d82c8c40eb595e7c9cd34
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-11-06 19:50:09 +0100

    chore: fix stated version numbers

--- init.ts
+++ init.ts
@@ -5,7 +5,7 @@ import { ask } from "./ask.ts";
 
 await new Command()
   .name("mod")
-  .version("v0.1.0")
+  .version("v1.0.1")
   .description("Start a new Deno project with a single command")
   .option(
     "-c, --config [config:boolean]",
