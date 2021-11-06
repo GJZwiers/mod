e81b69b2cb8e2af0b99f785480073fc715d04900
Geert-Jan Zwiers <34610306+GJZwiers@users.noreply.github.com>
2021-11-06 20:32:47 +0100

    chore: update readme and bump versions (#6)
    * chore: update readme and bump versions
    
    * fix: fmt

--- init.ts
+++ init.ts
@@ -5,7 +5,7 @@ import { ask } from "./ask.ts";
 
 await new Command()
   .name("mod")
-  .version("v1.0.1")
+  .version("v1.0.3")
   .description("Start a new Deno project with a single command")
   .option(
     "-c, --config [config:boolean]",
