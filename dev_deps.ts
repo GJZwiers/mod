0e557a72f66003e84ae823542c4a4d8b34a10cae
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-11-06 21:28:25 +0100

    chore(deps): bump deno std to 0.113.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,5 +2,5 @@ export {
   assert,
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.112.0/testing/asserts.ts";
+} from "https://deno.land/std@0.113.0/testing/asserts.ts";
 export { default as sinon } from "https://cdn.skypack.dev/sinon@11.1.2?dts";
