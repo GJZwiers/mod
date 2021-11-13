259fbc7d752c5b26b2f0fe20da2298a323e70317
Geert-Jan Zwiers <34610306+GJZwiers@users.noreply.github.com>
2021-11-13 12:15:54 +0100

    chore: use std/log for warnings instead of console.warn (#39)

--- dev_deps.ts
+++ dev_deps.ts
@@ -4,3 +4,4 @@ export {
   assertThrows,
 } from "https://deno.land/std@0.113.0/testing/asserts.ts";
 export { default as sinon } from "https://cdn.skypack.dev/sinon@11.1.2?dts";
+export * as log from "https://deno.land/std@0.114.0/log/mod.ts";
