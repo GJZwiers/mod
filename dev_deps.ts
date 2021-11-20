fadc88380e3ad86cbe33c2ffee53b57fdad51848
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-11-20 12:47:44 +0100

    chore: update std versions

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,6 +2,6 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.113.0/testing/asserts.ts";
+} from "https://deno.land/std@0.115.0/testing/asserts.ts";
 export { default as sinon } from "https://cdn.skypack.dev/sinon@11.1.2?dts";
-export * as log from "https://deno.land/std@0.114.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.115.0/log/mod.ts";
