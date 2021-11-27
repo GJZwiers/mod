5cc9662f006f5a32c53138c77905fe3a6b14e7c3
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-11-27 12:00:24 +0100

    chore: up version

--- init.ts
+++ init.ts
@@ -5,7 +5,7 @@ import { ask } from "./ask.ts";
 
 await new Command()
   .name("mod")
-  .version("v2.2.3")
+  .version("v2.2.4")
   .description("Start a new Deno project with a single command")
   .option(
     "--js [js:boolean]",
