0efcaa1a5bd420e6cc2921740b095c4a1b2b0445
Geert-Jan Zwiers <34610306+GJZwiers@users.noreply.github.com>
2021-12-29 19:16:40 +0100

    chore(deps): update deno std to 0.119.0 (#92)

--- init.ts
+++ init.ts
@@ -6,7 +6,7 @@ import { validateOptions } from "./validate_options.ts";
 
 await new Command()
   .name("mod")
-  .version("v2.2.11")
+  .version("v2.2.12")
   .description("Start a new Deno module with a single command")
   .option(
     "--js [js:boolean]",
