0f43c80f21f225b2d301001a35996e185fbc920c
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-02-15 19:37:16 +0100

    chore(deps): upgrade sinon and deno.land/std

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,6 +2,6 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.123.0/testing/asserts.ts";
-export { default as sinon } from "https://cdn.skypack.dev/sinon@12.0.1?dts";
-export * as log from "https://deno.land/std@0.123.0/log/mod.ts";
+} from "https://deno.land/std@0.125.0/testing/asserts.ts";
+export { default as sinon } from "https://cdn.skypack.dev/sinon@13.0.1?dts";
+export * as log from "https://deno.land/std@0.125.0/log/mod.ts";
