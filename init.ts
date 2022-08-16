eedebd8708340a532737a6c022c1a3258ff9b923
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-08-16 20:43:36 +0200

    chore: patch version (#116)

--- init.ts
+++ init.ts
@@ -10,7 +10,7 @@ import { asciiDeno } from "./ascii.ts";
  */
 await new Command()
   .name("mod")
-  .version("v2.3.7")
+  .version("v2.3.8")
   .description("Start a new Deno module with a single command")
   .option(
     "--js [js:boolean]",
